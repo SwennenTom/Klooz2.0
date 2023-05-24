9cccf8615a5af3c10188cf856530008147d3f4d2
TomSwennen <tswennen@gmail.com>
2023-05-24 10:56:08 +0200

    addworkonExperimenten

--- script/script.js
+++ script/script.js
@@ -1,23 +1,29 @@
 window.addEventListener('load', function() {
-    var buttons = document.querySelectorAll('button');
-    var delay = 100; // milliseconds
-    var transitionTime = 250; // milliseconds
-  
-    for (var i = 0; i < buttons.length; i++) {
-      (function(index) {
+  var buttons = document.querySelectorAll('button');
+  var delay = 100; // milliseconds
+  var transitionTime = 250; // milliseconds
+
+  for (var i = 0; i < buttons.length; i++) {
+    (function(index) {
+      setTimeout(function() {
+        buttons[index].classList.add('green-button');
         setTimeout(function() {
-          buttons[index].classList.add('green-button');
-          setTimeout(function() {
-            buttons[index].style.transition = "all " + transitionTime + "ms ease-in-out";
-            buttons[index].classList.remove('green-button');
-          }, delay);
-        }, delay * index);
+          buttons[index].style.transition = "all " + transitionTime + "ms ease-in-out";
+          buttons[index].classList.remove('green-button');
+        }, delay);
+      }, delay * index);
 
-        buttons[index].addEventListener('click', function() {
-            var topElement = document.getElementById("top");
-            topElement.scrollIntoView({ behavior: "smooth" });
-        });
+      buttons[index].addEventListener('click', function() {
+          var topElement = document.getElementById("top");
+          topElement.scrollIntoView({ behavior: "smooth" });
+      });
 
-      })(i);
-    }
+    })(i);
+  }
 });
+
+function flipCard(card) {
+  card.classList.toggle('flipped');
+}
+
+
